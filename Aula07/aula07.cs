a2a6992c8ffa481123f8a6151ea08256bce21f8e
Bruno Santos <bruno.if@hotmail.com>
2025-11-25 00:38:28 -0300

    Aula 07

---
+++ Aula07/aula07.cs
@@ -0,0 +1,14 @@
+using System;
+
+//Constantes
+class Aula07
+{
+    static void Main()
+    {
+        const string canal = "CFB Cursos";
+        const double pi = 3.1415;
+
+        Console.WriteLine("Canal: " + canal);
+        Console.WriteLine("Valor de Pi: " + pi);
+    }
+}
