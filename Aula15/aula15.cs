920b515c0aa3b4820064b3bef79fa4c73f3f6ce5
Bruno Santos <bruno.if@hotmail.com>
2025-11-25 03:10:04 -0300

    Aula 15

---
+++ Aula15/aula15.cs
@@ -0,0 +1,39 @@
+using System;
+
+//Estudando Switch case
+
+class Aula15
+{
+    static void Main()
+    {
+        int diaSemana = 3;
+
+        switch (diaSemana)
+        {
+            case 1:
+                Console.WriteLine("Domingo");
+                break;
+            case 2:
+                Console.WriteLine("Segunda-feira");
+                break;
+            case 3:
+                Console.WriteLine("Terça-feira");
+                break;
+            case 4:
+                Console.WriteLine("Quarta-feira");
+                break;
+            case 5:
+                Console.WriteLine("Quinta-feira");
+                break;
+            case 6:
+                Console.WriteLine("Sexta-feira");
+                break;
+            case 7:
+                Console.WriteLine("Sábado");
+                break;
+            default:
+                Console.WriteLine("Dia inválido");
+                break;
+        }
+    }
+}
