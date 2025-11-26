121d6f051fc754c8e9700ae7f7f44c40a64322ab
Bruno Santos <bruno.if@hotmail.com>
2025-11-26 10:30:50 -0300

    Aula 24

---
+++ Aula24/aula24.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Aula24
+{
+    static void Main()
+    {
+        ola();
+        printarnumero(10);
+        printarnumero(20);
+    }
+
+    static void Ola()
+    {
+        Console.WriteLine("Olá Mundo!");
+    }
+
+    static void Printarnumero(int n)
+    {
+        Console.WriteLine("Número: " + n);
+    }
+}
